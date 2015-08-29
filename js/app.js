89b33fe2971230dde6c02e8be0dedd16b670ab5e
kongou-ae <y.matsumoto.ae@gmail.com>
2015-08-30 00:42:30 +0900

    #9

--- js/app.js
+++ js/app.js
@@ -4,27 +4,45 @@
 angular.module('hogemine', ['ngResource','ngRoute'])
     .config(
         function ($routeProvider){
-        $routeProvider
-            .when('/', {
-                templateUrl: 'view/project.html',
-                controller: 'projectController'
-            })
-            .when('#/', {
-                templateUrl: 'view/project.html',
-                controller: 'projectController'
-            })
-            .when('/setting', {
-                templateUrl: 'view/setting.html',
-                controller: 'settingController'
-            })
-            .when('/project/:projectId', {
-                templateUrl: 'view/ticket.html',
-                controller: 'ticketController'
-            })
-            .when('/project/:projectId/issue/:issueId', {
-                templateUrl: 'view/ticket_detail.html',
-                controller: 'ticketController'
-            });
+            var apiKey = window.localStorage.getItem('redmineApi');
+            var redmineUrl = window.localStorage.getItem('redmineUrl');
+
+            $routeProvider
+                .when('/setting', {
+                    templateUrl: 'view/setting.html',
+                    controller: 'settingController'
+                })
+                .when('/project/:projectId', {
+                    templateUrl: 'view/ticket.html',
+                    controller: 'ticketController'
+                })
+                .when('/project/:projectId/issue/:issueId', {
+                    templateUrl: 'view/ticket_detail.html',
+                    controller: 'ticketController'
+                });
+            
+            console.log(apiKey)
+            if (apiKey == null || redmineUrl == null) {
+                 $routeProvider
+                    .when('/', {
+                        templateUrl: 'view/setting.html',
+                        controller: 'settingController'
+                    })
+                    .when('#/', {
+                        templateUrl: 'view/setting.html',
+                        controller: 'settingController'
+                    });
+            } else {
+                $routeProvider
+                    .when('/', {
+                        templateUrl: 'view/project.html',
+                        controller: 'projectController'
+                    })
+                    .when('#/', {
+                        templateUrl: 'view/project.html',
+                        controller: 'projectController'
+                    });
+            }
             //.otherwise({
             //    redirectTo: 'login'
             //});
