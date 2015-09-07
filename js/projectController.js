54c009630caa124f4ae3e0d77eee4df955138c81
kongou-ae <y.matsumoto.ae@mail.com>
2015-09-07 12:46:06 +0900

    show projects over 25

--- js/projectController.js
+++ js/projectController.js
@@ -5,6 +5,7 @@ angular.module('hogemine')
         var apiKey = window.localStorage.getItem('redmineApi');
         var redmineUrl = window.localStorage.getItem('redmineUrl');
         var count = "";
+        var projectAry = [];
 
         $http.defaults.headers.common["X-Redmine-API-Key"] = apiKey;
 
@@ -12,38 +13,29 @@ angular.module('hogemine')
         $http.get('http://localhost:8080/'+redmineUrl +'projects.json').success(function(data){
             count =  Math.ceil(data.total_count/data.limit);
 
-            if (count = 1) {
-                $http.get('http://localhost:8080/'+redmineUrl +'projects.json').success(function(data){
-                    $scope.projects = data;
-
-                    $scope.currentPage = 0;
-                    $scope.pageSize = 20;
-                    $scope.data = [];
-                    $scope.numberOfPages=function(){
-                        return Math.ceil($scope.data.length/$scope.pageSize);
-                    };
-
-                    for (var i=0; i<data.total_count; i++) {
-                        $scope.data.push("Item "+i);
-                    }
+            // データを表示する
+            for (var i = 1; i <= count; i++) {
+                $http.get('http://localhost:8080/'+redmineUrl +'projects.json'+'?page=' + i).success(function(data){
+                      for (var j = 0; j < data.projects.length; j++){
+                          projectAry.push(data.projects[j])
+                      }
+                      $scope.projects = projectAry;
+                      console.log(projectAry)
                 })
-            } else {
-                for (var i = 1; i <= count; i++) {
-                    $http.get('http://localhost:8080/'+redmineUrl +'projects.json'+'&page=' + i).success(function(data){
-                        $scope.projects = data;
+            }
+        })
+        // プロジェクトのトータル数を取得し、ページ数の計算に利用する
+        $http.get('http://localhost:8080/'+redmineUrl +'projects.json').success(function(data){
 
-                        $scope.currentPage = 0;
-                        $scope.pageSize = 20;
-                        $scope.data = [];
-                        $scope.numberOfPages=function(){
-                            return Math.ceil($scope.data.length/$scope.pageSize);
-                        };
+            $scope.currentPage = 0;
+            $scope.pageSize = 20;
+            $scope.data = [];
+            $scope.numberOfPages=function(){
+                return Math.ceil($scope.data.length/$scope.pageSize);
+            };
 
-                        for (var i=0; i<data.total_count; i++) {
-                            $scope.data.push("Item "+i);
-                        }
-                    })
-                }
+            for (var i=0; i<data.total_count; i++) {
+                $scope.data.push("Item "+i);
             }
         })
     })
