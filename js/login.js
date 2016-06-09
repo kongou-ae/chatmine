cfb6b2b3ce8f6f17cde6108ebdcaca81016694ec
kongou-ae <y.matsumoto.ae@gmail.com>
2016-06-09 22:48:36 +0900

    add saving of close code
    #46

--- js/login.js
+++ js/login.js
@@ -19,11 +19,16 @@ login.model.saveUsername = function(Username){
     localStorage.setItem('redmineUsername', Username )
 }
 
+login.model.saveCloseid = function(closeId){
+    localStorage.setItem('redmeineCloseid', closeId )
+}
+
 login.model.loadCredential = function(){
     var credential = {}
     credential.url = localStorage.getItem('redmineUrl')
     credential.key = localStorage.getItem('redmineApiKey')
     credential.userName = localStorage.getItem('redmineUsername')
+    credential.closeId = localStorage.getItem('redmeineCloseid')
     return credential
 }
 
@@ -32,7 +37,11 @@ login.vm = {
     init: function(){
 
         //todo:認証情報が保存されているときのログインフローを直す
-        var credential = login.model.loadCredential()
+        var credential = login.model.loadCredential();
+
+        login.vm.loginPhase = m.prop("0");
+        login.vm.closeId = m.prop("");
+        login.vm.issuesStatus = m.prop([]);
 
         if (credential.url){
             login.vm.redmineUrl = m.prop(credential.url);
@@ -46,10 +55,17 @@ login.vm = {
             login.vm.redmineApiKey = m.prop("");
         }
 
-        login.vm.credentialError = m.prop("");
+        login.vm.buildCloseidarray = function(a){
+            var tmp = []
+            a.map(function(status,idx){
+                if ((status.is_closed !== undefined ) && (status.is_closed == true)){
+                    tmp.push(status)
+                }
+            })
+            return tmp
+        }
 
         login.vm.submit = function(){
-            login.vm.credentialError("")
             login.model.saveUrl(login.vm.redmineUrl())
             login.model.saveApiKey(login.vm.redmineApiKey())
 
@@ -64,39 +80,79 @@ login.vm = {
                 url: "http://localhost:8080/" + credential.url + "/users/current.json",
                 config: redmineApiKey,
                 unwrapSuccess: function(a,b) {
-                    login.model.saveUsername(a.user.login)
-                    m.route("/project")
-                }
+                    // ユーザ情報を格納
+                    login.model.saveUsername(a.user.login)                    
+                    m.request({
+                        method: "GET",
+                        url:"http://localhost:8080/" + credential.url + "/issue_statuses.json",
+                        config: redmineApiKey,
+                        unwrapSuccess: function(a,b) {
+                            // ステータスIDを保存
+                            login.vm.issuesStatus(login.vm.buildCloseidarray(a.issue_statuses))
+                            login.vm.loginPhase = m.prop("1");                 
+                        }
+                    }) 
+               }
             })
+        }
+
+        login.vm.submit2 = function(){
+            if ( login.vm.closeId() !== undefined ){
+                login.model.saveCloseid(login.vm.closeId())
+                m.route("/project")            
+            }
         }
     }
 }
 
 login.controller = function(){
-    var credential = login.model.loadCredential()
-    if (credential.url){
-        if (credential.key.length == 40){
-            var redmineApiKey = function(xhr) {
-                xhr.setRequestHeader("X-Redmine-API-Key", credential.key);
-            };
-
-            m.request({
-                method: "GET",
-                url: "http://localhost:8080/" + credential.url + "/projects.json",
-                config: redmineApiKey,
-                unwrapSuccess: function(a,b) {
-                    m.route("/project")
-                },
-                unwrapError: function(a,b) {
-                    login.vm.init()
-                }
-            })
-        }
+    var credential =  login.model.loadCredential()
+    var redmineApiKey = function(xhr) {
+        xhr.setRequestHeader("X-Redmine-API-Key", credential.key);
+    };
+    if (credential.url && credential.key.length === 40 && credential.closeId ){
+        m.request({
+            method: "GET",
+            url: "http://localhost:8080/" + credential.url + "/users/current.json",
+            config: redmineApiKey,
+            unwrapSuccess: function(a,b) {
+                m.route("/project")                          
+            }
+        })
     }
     login.vm.init()
 }
 
 login.view = function() {
+    
+    login.view.selectCloseid = function(){
+
+        if (login.vm.loginPhase() == "1" ){
+            return [
+                m("div.settingFrom",[
+                    m("div.form-horizontal",[
+                        m("div.form-group",[
+                            m("label","the close code"),
+                            m("select.form-control",{
+                                onchange:m.withAttr("value",login.vm.closeId)
+                            },[
+                                m("option",{value:""},"select close code"),
+                                login.vm.issuesStatus().map(function(status,idx){
+                                    return m("option",{value:status.id},status.name )                                    
+                                })
+                            ]),
+                        ]),
+                        m("button.btn btn-default",{
+                            //type:'submit',
+                            // bind(this)がないと、クリック前に保存が発動する
+                            onclick: login.vm.submit2.bind(this)
+                        },"Start!!")
+                    ])
+                ])
+            ]
+        }
+    }
+    
     return [
             m("div.sidebar-fixed",[
                 m("h1",[
@@ -108,34 +164,29 @@ login.view = function() {
                 m("h2","Setting"),
                 m("div.settingFrom",[
                     //m("form",{method:'POST',enctype:'multipart/form-data',acceptCharset:'UTF-8'},[
-                    m("form",[
+                    m("div.form-horizontal",[
                         m("div.form-group",[
-                            m("div.form-inline",[,
                                 m("label","Redmine URL"),
-                                m("input",{
+                                m("input.form-control",{
                                     oninput:m.withAttr("value", login.vm.redmineUrl),
                                     value:login.vm.redmineUrl()
                                 })
-                            ]),
-                            m("div.form-inline",[
+                        ]),
+                        m("div.form-group",[
                                 m("label","Redmine API Key"),
-                                m("input",{
+                                m("input.form-control",{
                                     oninput:m.withAttr("value", login.vm.redmineApiKey),
                                     value:login.vm.redmineApiKey()
-                                })
-                            ]),
-                            m("div.form-inline",[
-                                m("button",{
-                                    //type:'submit',
-                                    // bind(this)がないと、クリック前に保存が発動する
-                                    onclick: login.vm.submit.bind(this)
-                                },"Start!")
-                            ])
-                        ])
+                                }),
+                        ]),                     
+                        m("button.btn btn-default",{
+                            //type:'submit',
+                            // bind(this)がないと、クリック前に保存が発動する
+                            onclick: login.vm.submit.bind(this)
+                        },"Check the credential!")
                     ]),
-                    credential.view()
-                ])
-
+                ]),
+                login.view.selectCloseid(),
             ])
            ]
 };
